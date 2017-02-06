d37c21af8c79ea2157c87aec6d1b85bee02c9495
Dan Persa <dan.persa@gmail.com>
2017-02-06 13:46:51 +0100

    Fix the xhr support; Add first acceptance tests

--- src/core/async/http/client/xhr.cljs
+++ src/core/async/http/client/xhr.cljs
@@ -1,11 +1,18 @@
 (ns core.async.http.client.xhr
   (:refer-clojure :exclude [get])
   (:require [goog.net.XhrIo :as xhr]
+            [goog.events :as events]
             [cljs.core.async :refer [chan close! >! <!]]
             [core.async.http.client :as c]
             [core.async.http.protocols :as proto])
   (:require-macros [cljs.core.async.macros :refer [go alt!]])
-  (:import [goog.net ErrorCode EventType]))
+  (:import [goog.net ErrorCode EventType XhrIo]))
+
+(defn close-chans [chans]
+  (close! (chans :status-chan))
+  (close! (chans :headers-chan))
+  (close! (chans :body-chan))
+  (close! (chans :error-chan)))
 
 (def client
   (reify proto/Client
@@ -22,32 +29,64 @@
       (let [chans {:status-chan  (or status-chan (chan 1))
                    :headers-chan (or headers-chan (chan 1))
                    :body-chan    (or body-chan (chan 1024))
-                   :error-chan   (or error-chan (chan 1))}]
-        (xhr/send
-          url
-          (fn [event]
-            (condp = (.-type event)
-              EventType.COMPLETE
-              (let [response (-> event
-                                 .-target)
-                    body (.getResponse response)
-                    headers (-> (.getResponseHeaders response)
-                                (js->clj))
-                    status (.getStatus response)]
-                (go
-                  (>! (chans :status-chan) status)
-                  (>! (chans :headers-chan) headers)
-                  (>! (chans :body-chan) body)))
-              EventType.TIMEOUT
-              (do
-                (close! (chans :status-chan))
-                (close! (chans :headers-chan))
-                (close! (chans :body-chan))
-                (go (>! (chans :error-chan) :timeout)))))
-          (c/convert-method-name method)
-          body
-          (clj->js headers)
-          timeout)
+                   :error-chan   (or error-chan (chan 1))}
+            xhrio (XhrIo.)]
+
+        (comment (.setProgressEventsEnabled xhrio true))
+
+        (events/listen xhrio EventType.TIMEOUT
+                       (fn [event]
+                         ; (println "Event type: " (.-type event))
+                         (go
+                           (>! (chans :error-chan) :timeout)
+                           (close-chans chans))))
+        (events/listen xhrio EventType.ERROR
+                       (fn [event]
+                         ; (println "Event type: " (.-type event))
+                         (let [response (-> event
+                                            .-target)
+                               body (.getResponse response)
+                               headers (-> (.getResponseHeaders response)
+                                           (js->clj))
+                               status (.getStatus response)]
+                           (go
+                             (>! (chans :status-chan) status)
+                             (>! (chans :headers-chan) headers)
+                             (>! (chans :body-chan) body)
+                             (>! (chans :error-chan) :error)
+                             (close-chans chans)))))
+        (comment
+          (events/listen xhrio EventType.PROGRESS
+                         (fn [event]
+                           (println "Event type: " (.-type event) " " (.-loaded event) " " (.-total event))))
+          (events/listen xhrio EventType.ABORT
+                         (fn [event]
+                           (println "Event type: " (.-type event)))))
+        (events/listen xhrio EventType.SUCCESS
+                       (fn [event]
+                         ; (println "Event type: " (.-type event))
+                         (let [response (-> event
+                                            .-target)
+                               body (.getResponse response)
+                               headers (-> (.getResponseHeaders response)
+                                           (js->clj))
+                               status (.getStatus response)]
+                           (go
+                             (>! (chans :status-chan) status)
+                             (>! (chans :headers-chan) headers)
+                             (>! (chans :body-chan) body)
+                             (close-chans chans)))))
+        (comment
+          (events/listen xhrio EventType.COMPLETE
+                         (fn [event]
+                           (println "Event type: " (.-type event)))))
+        (.setTimeoutInterval xhrio timeout)
+        (.send xhrio
+               url
+               (c/convert-method-name method)
+               body
+               (clj->js headers))
+
         {:status  (chans :status-chan)
          :headers (chans :headers-chan)
          :body    (chans :body-chan)
--- test/core/async/http/client/xhr_test.cljs
+++ test/core/async/http/client/xhr_test.cljs
@@ -5,15 +5,53 @@
             [devcards.core :refer-macros [deftest]]
             [cljs.core.async :refer [<! >!]]))
 
-(deftest get-test
-         (testing "successful get"
+(def ^:private endpoints-url "http://localhost:8083")
+
+(deftest ^:acceptance get-test
+         (testing "Successful async get"
            (async done
-             (let [response (http/get "http://localhost:8083/endpoint-1")
+             (let [response (http/get (str endpoints-url "/endpoint-1"))
                    body-chan (response :body)
                    status-chan (response :status)]
                (go
-                 (let [status (<! status-chan)
-                       body (<! body-chan)]
-                   (is (= 200 status))
-                   (is (= "Hello world and endpoint-1" body))
-                   (done)))))))
+                 (is (= 200 (<! status-chan)))
+                 (is (= "Hello world and endpoint-1" (<! body-chan)))
+                 (done))))))
+
+(deftest ^:acceptance get-from-streaming-endpoint-test
+         (testing "Successful async get from a streaming endpoint"
+           (async done
+             (let [response (http/get (str endpoints-url "/streaming-endpoint-1"))
+                   body-chan (response :body)
+                   status-chan (response :status)]
+               (go
+                 (is (= 200 (<! status-chan)))
+                 ; not really streaming, as xhr doesn't seem to really support it
+                 (is (= "streaming-endpoint-1 part-0 part-1 part-2" (<! body-chan)))
+                 (done))))))
+
+(deftest ^:acceptance error-enpoint
+         (testing "Do an async get to an endpoint which returns 500"
+           (async done
+             (let [response (http/get (str endpoints-url "/error"))
+                   body-chan (response :body)
+                   status-chan (response :status)]
+               (go
+                 (is (= 500 (<! status-chan)))
+                 (is (= "Hello world 500" (<! body-chan)))
+                 (is (= nil (<! body-chan)))
+                 (done))))))
+
+(deftest ^:acceptance timeout-endpoint
+         (testing "Successful async get to an endpoint which times out"
+           (async done
+             (let [response (http/get (str endpoints-url "/sleep")
+                                      {:timeout 100})
+                   body-chan (response :body)
+                   status-chan (response :status)
+                   error-chan (response :error)]
+               (go
+                 (is (= :timeout (<! error-chan)))
+                 (is (= nil (<! status-chan)))
+                 (is (= nil (<! body-chan)))
+                 (done))))))
